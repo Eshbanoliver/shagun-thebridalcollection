5670f4472a53dede9504a522a3f329cbe411cb67
Eshbanoliver <eshbanoliver@gmail.com>
2026-04-02 10:26:52 +0530

    contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -200,22 +200,16 @@ const Contact: React.FC = () => {
             light
           />
           <div className="contact-map">
-            <div className="contact-map__placeholder">
-              <div className="contact-map__icon">
-                <MapPin size={48} />
-              </div>
-              <h3>Shagun THE BRIDAL COLLECTION</h3>
-              <p>Orbit Garden Road, Ganpati Vihar, Pulan, Pulla Bhuwana,<br />Pulla, Rupsagar, Udaipur, Rajasthan 313001</p>
-              <a
-                href="https://maps.google.com/?q=Orbit+Garden+Road+Ganpati+Vihar+Udaipur+Rajasthan+313001"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="btn btn-gold contact-map__btn"
-                id="contact-map-btn"
-              >
-                Open in Google Maps
-              </a>
-            </div>
+            <iframe 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3627.6005493429643!2d73.69800717536272!3d24.60297817809823!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3967e52cac4fc73b%3A0x65b42682a08d5a2b!2sSHAGUN%20The%20Bridal%20Collection!5e0!3m2!1sen!2sin!4v1775105735942!5m2!1sen!2sin" 
+              width="100%" 
+              height="450" 
+              style={{ border: 0 }} 
+              allowFullScreen={true} 
+              loading="lazy" 
+              referrerPolicy="no-referrer-when-downgrade"
+              title="Shagun The Bridal Collection Location"
+            ></iframe>
           </div>
         </div>
       </section>
